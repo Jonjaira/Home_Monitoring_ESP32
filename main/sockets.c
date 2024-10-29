7283cc1f1ba5a5cbc237755b3a9b74ccfb9c9dbe
Jessica <jonjaira@gmail.com>
2024-10-28 20:35:22 -0400

    Multple clients accepted

--- main/sockets.c
+++ main/sockets.c
@@ -1,11 +1,3 @@
-/* BSD Socket API Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <string.h>
 #include <sys/param.h>
 #include "freertos/FreeRTOS.h"
@@ -16,21 +8,21 @@
 #include "esp_log.h"
 #include "nvs_flash.h"
 #include "esp_netif.h"
-
 #include "lwip/err.h"
 #include "lwip/sockets.h"
 #include "lwip/sys.h"
 #include <lwip/netdb.h>
 
-
 #define PORT                        3333
 #define KEEPALIVE_IDLE              5
 #define KEEPALIVE_INTERVAL          5
 #define KEEPALIVE_COUNT             3
+#define MAX_CLIENTS                 10  // Maximum simultaneous client connections
 
 static const char *TAG = "example";
 
-static void do_retransmit(const int sock)
+// Function to handle receiving data from each client
+static void receive_data(const int sock)
 {
     int len;
     char rx_buffer[128];
@@ -42,23 +34,22 @@ static void do_retransmit(const int sock)
         } else if (len == 0) {
             ESP_LOGW(TAG, "Connection closed");
         } else {
-            rx_buffer[len] = 0; // Null-terminate whatever is received and treat it like a string
+            rx_buffer[len] = 0; // Null-terminate received data
             ESP_LOGI(TAG, "Received %d bytes: %s", len, rx_buffer);
-
-            // send() can return less bytes than supplied length.
-            // Walk-around for robust implementation.
-            int to_write = len;
-            while (to_write > 0) {
-                int written = send(sock, rx_buffer + (len - to_write), to_write, 0);
-                if (written < 0) {
-                    ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
-                    // Failed to retransmit, giving up
-                    return;
-                }
-                to_write -= written;
-            }
         }
     } while (len > 0);
+}
+
+// New task to handle each client connection
+void handle_client_task(void *pvParameters) {
+    int sock = (int)pvParameters;
+
+    receive_data(sock);
+
+    shutdown(sock, 0);
+    close(sock);
+
+    vTaskDelete(NULL); // Delete the task once done
 }
 
 void tcp_server_task(void *pvParameters)
@@ -69,9 +60,9 @@ void tcp_server_task(void *pvParameters)
     int addr_family = (int)pvParameters;
     int ip_protocol = 0;
     int keepAlive = 1;
-    int keepIdle = 5;
-    int keepInterval = 5;
-    int keepCount = 3;
+    int keepIdle = KEEPALIVE_IDLE;
+    int keepInterval = KEEPALIVE_INTERVAL;
+    int keepCount = KEEPALIVE_COUNT;
     struct sockaddr_storage dest_addr;
 
     if (addr_family == AF_INET) {
@@ -96,22 +87,21 @@ void tcp_server_task(void *pvParameters)
     int err = bind(listen_sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
     if (err != 0) {
         ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
-        ESP_LOGE(TAG, "IPPROTO: %d", addr_family);
         goto CLEAN_UP;
     }
     ESP_LOGI(TAG, "Socket bound, port %d", PORT);
 
-    err = listen(listen_sock, 1);
+    // Increase backlog to allow multiple queued connections
+    err = listen(listen_sock, MAX_CLIENTS);
     if (err != 0) {
         ESP_LOGE(TAG, "Error occurred during listen: errno %d", errno);
         goto CLEAN_UP;
     }
 
     while (1) {
-
         ESP_LOGI(TAG, "Socket listening");
 
-        struct sockaddr_storage source_addr; // Large enough for both IPv4 or IPv6
+        struct sockaddr_storage source_addr;
         socklen_t addr_len = sizeof(source_addr);
         int sock = accept(listen_sock, (struct sockaddr *)&source_addr, &addr_len);
         if (sock < 0) {
@@ -119,23 +109,22 @@ void tcp_server_task(void *pvParameters)
             break;
         }
 
-        // Set tcp keepalive option
+        // Set TCP keepalive options for the accepted socket
         setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, &keepAlive, sizeof(int));
         setsockopt(sock, IPPROTO_TCP, TCP_KEEPIDLE, &keepIdle, sizeof(int));
         setsockopt(sock, IPPROTO_TCP, TCP_KEEPINTVL, &keepInterval, sizeof(int));
         setsockopt(sock, IPPROTO_TCP, TCP_KEEPCNT, &keepCount, sizeof(int));
         
-        // Convert ip address to string
         if (source_addr.ss_family == PF_INET) {
             inet_ntoa_r(((struct sockaddr_in *)&source_addr)->sin_addr, addr_str, sizeof(addr_str) - 1);
         }
+        ESP_LOGI(TAG, "Socket accepted, IP address: %s", addr_str);
 
-        ESP_LOGI(TAG, "Socket accepted ip address: %s", addr_str);
-
-        do_retransmit(sock);
-
-        shutdown(sock, 0);
-        close(sock);
+        // Create a new task to handle each client connection
+        if (xTaskCreate(handle_client_task, "handle_client", 4096, (void *)sock, tskIDLE_PRIORITY, NULL) != pdPASS) {
+            ESP_LOGE(TAG, "Failed to create task for handling client");
+            close(sock);
+        }
     }
 
 CLEAN_UP:
